cc6dab8275c77c6292f4729a8a6fa2e2a453434a
AlexandreUff <alexandre_magno@id.uff.br>
2024-01-22 17:27:58 -0300

    feat: add more items

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,55 @@
 /* import { useState } from 'react' */
-import './App.css'
-import Chart from './components/Charts'
-import GraphicTypeContent from './components/GraphicTypeContent'
-import { Logo, Upload } from './components/Icons'
-import Metrics from './components/Metrics'
+import "./App.css";
+import Chart from "./components/Charts";
+import GraphicTypeContent from "./components/GraphicTypeContent";
+import { Logo, Upload } from "./components/Icons";
+import Metrics from "./components/Metrics";
 
 function App() {
-
   return (
     <>
       <header>
         <Logo /> myStonks
       </header>
       <main>
-        <Metrics
-          types={
-            [
+        <div className="sheet-viewer-content">
+          <Metrics
+            types={[
               {
-                name: "MRR"
+                name: "MRR",
               },
               {
-                name: "Churn Rate"
+                name: "Churn Rate",
               },
-            ]
-          }
-        />
-        <Chart />
-        <GraphicTypeContent
-          types={
-            [
+            ]}
+          />
+          <Chart />
+          <GraphicTypeContent
+            types={[
               {
-                name: "Line"
+                name: "Line",
               },
               {
-                name: "Pizza"
+                name: "Pizza",
               },
               {
-                name: "Circle"
+                name: "Circle",
               },
-            ]
-          }
-        />
+            ]}
+          />
+        </div>
+        <div className="sheet-controls-content">
+        <button className="upload-button" title="Subir nova planilha">
+          <Upload /> Subir nova planilha
+        </button>
+        </div>
       </main>
-      <button className='upload-button' title='Subir nova planilha'>
-        <Upload />  Subir nova planilha
-      </button>
       <footer>
         <small>© 2024 - Alexandre Magno Dias</small>
         <small>alexandre.magno.wrk@gmail.com</small>
       </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
